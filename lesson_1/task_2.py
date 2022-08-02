0070d11009685bf19a9171c4c96f4383bf8f70ae
optikrus <optikrus61@gmail.com>
2022-08-02 18:02:23 +0300

    improve functions

--- lesson_1/task_2.py
+++ lesson_1/task_2.py
@@ -1,24 +1,29 @@
 import asyncio
 from asyncio.subprocess import PIPE
+import locale
 import platform
-from ipaddress import ip_address, IPv4Address
-from task_1 import ENCODING
+from ipaddress import ip_address
+from typing import Union
+
+ENCODING = locale.getpreferredencoding()
 
 
-async def ping_ip(ip_addr: IPv4Address) -> tuple[bool, IPv4Address]:
-    param = '-n' if platform.system().lower() == 'windows' else '-c'
-    cmd = f'ping {param} 5 {ip_addr}'
-    proc = await asyncio.create_subprocess_shell(cmd=cmd, stdout=PIPE, stderr=PIPE)
+async def host_ping_range(ip_addr: str, count: int) -> Union[list[tuple], str]:
+    if count < 257:
+        param = '-n' if platform.system().lower() == 'windows' else '-c'
+        cmd = f'ping {param} 5 {ip_addr}'
+        proc = await asyncio.create_subprocess_shell(cmd=cmd, stdout=PIPE, stderr=PIPE)
+        stdout, stderr = await proc.communicate()
 
-    stdout, stderr = await proc.communicate()
-    ip = ip_address(stdout.split()[2][1:-2].decode(ENCODING))
-    if proc.returncode == 0:
-        return True, ip
-    return False, ip
+        results = []
 
+        for i in range(count):
+            if proc.returncode:
+                availability = '-'
+            else:
+                availability = '+'
+            result = availability, str(ip_address(stdout.decode(ENCODING).split()[2][1:-3] + '0') + i)
+            results.append(result)
+        return results
 
-async def host_range_ping(ip_addr: str, count: int):
-    results = []
-    for i in range(count):
-        results.append(ping_ip(ip_address(ip_addr[:-1] + '0') + i))
-    return await asyncio.gather(*results)
+    return 'Значение count не более 256'
--- lesson_1/task_3.py
+++ lesson_1/task_3.py
@@ -1,12 +1,29 @@
-from ipaddress import ip_address
 import asyncio
+from asyncio.subprocess import PIPE
+import locale
+import platform
+from ipaddress import ip_address
 from tabulate import tabulate
-from task_2 import ping_ip
+
+ENCODING = locale.getpreferredencoding()
 
 
-async def host_range_ping_tab(ip_addr: str, count: int) -> str:
-    results = []
-    for i in range(count):
-        results.append(ping_ip(ip_address(ip_addr[:-1] + '0') + i))
-    result = await asyncio.gather(*results)
-    return tabulate(result, headers=('Доступность', 'IP-адрес'), tablefmt="grid")
+async def host_ping_range(ip_addr: str, count: int) -> str:
+    if count < 257:
+        param = '-n' if platform.system().lower() == 'windows' else '-c'
+        cmd = f'ping {param} 5 {ip_addr}'
+        proc = await asyncio.create_subprocess_shell(cmd=cmd, stdout=PIPE, stderr=PIPE)
+        stdout, stderr = await proc.communicate()
+
+        results = []
+
+        for i in range(count):
+            if proc.returncode:
+                availability = '-'
+            else:
+                availability = '+'
+            result = availability, str(ip_address(stdout.decode(ENCODING).split()[2][1:-3] + '0') + i)
+            results.append(result)
+        return tabulate(results, headers=('Доступность', 'IP-адрес'), tablefmt="grid", stralign="center")
+
+    return 'Значение count не более 256'
